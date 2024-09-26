c8bd91af46f406cc03c644b093468a5192fc60a0
cyberchrix <chenry59@gmail.com>
2024-09-26 18:57:06 +0200

    Updates

--- video-player/draft.js
+++
@@ -1,8 +0,0 @@
-const videoList = document.getElementById('video-list');
-const videoItems = videoList.getElementsByTagName('li');
-
-for (let videoItem of videoItems) {
-    videoItem.addEventListener('click', function() {
-        alert('click');
-    });
-};
